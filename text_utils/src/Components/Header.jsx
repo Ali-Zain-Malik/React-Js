c694f8ccab250a0713349ea2ee0cc711d55dbc1c
Ali Zain Malik <alizainmalik123@gmail.com>
2024-11-11 00:11:05 +0500

    Add dark mode in nav bar

--- text_utils/src/Components/Header.jsx
+++ text_utils/src/Components/Header.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import lightMode from "../Images/light-mode.svg";
+import darkMode from "../Images/dark-mode.svg";
 const Header = () => 
 {
+    const handleThemeToggle = () =>
+    {
+        setIsDarkMode(prevMode => !prevMode);
+    }
+    const [isDarkMode, setIsDarkMode] = useState(false);
     return (
-        <nav className="navbar bg-dark">
+        <nav className={`navbar ${isDarkMode ? 'dark-mode': 'light-mode'}`}>    
             <div className="container-fluid">
-                <span className="navbar-brand mb-0 h1 text-light">Navbar</span>
+                <span className={`navbar-brand mb-0 h1 ${isDarkMode ? "text-light" : "text-dark"}`}>Navbar</span>
+                <img src={isDarkMode ? lightMode : darkMode} onClick={handleThemeToggle} role="button" alt="" />
             </div>
         </nav>
     );
